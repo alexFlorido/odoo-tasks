661470ccba88ef9a3cb3091aea81641f4fa8afce
alexFlorido <alexflorido0897@gmail.com>
2023-07-19 08:47:58 -0400

    [MODIFY] Course model adding some Fields

--- models/course.py
+++ models/course.py
@@ -3,3 +3,16 @@ from odoo import fields, models
 class Course(models.Model):
     _name =  'course.course',
     _description = 'Course info'
+
+
+    name = fields.Char(string="Title", required=True)
+    active = fields.Boolean(string="Active", default=True)
+
+    description = fields.Text()
+    level = fields.Selection(string="Level",
+                             selection=[
+                                 ('beginner', "Beginner")
+                                 ('intermediate', "Intermediate")
+                                 ('advanced', "Advanced")
+                             ],
+                             copy=False)
