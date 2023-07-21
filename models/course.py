a9f5ad75335b6deec1196a36eedebb6902552a77
alexFlorido <alexflorido0897@gmail.com>
2023-07-21 16:03:26 -0400

    [ADD] New Advanced Views (Calendar and Pivot)

--- models/course.py
+++ models/course.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 class Course(models.Model):
     _name="academy.course"
@@ -15,3 +16,16 @@ class Course(models.Model):
                                  ('advanced', 'Advanced'),
                              ],
                              copy=False)
+    
+    session_ids = fields.One2many(comodel_name="academy.session", string="Session", inverse_name="course_id")
+    currency_id = fields.Many2one(comodel_name="res.currency", string="Currency", default=lambda self:self.env.company.currency_id.id)
+    base_price = fields.Monetary(string="Base Price", currency_field="currency_id")
+    additional_fee = fields.Monetary(string="Additional Fee", currency_field="currency_id", default="0")
+    total_price = fields.Monetary(string="Total Price", currency_field="currency_id", compute="_compute_total_price", store=True)
+
+    @api.depends("base_price", "additional_fee")
+    def _compute_total_price(self):
+        for record in self:
+            if(record.base_price <0):
+                raise ValidationError("Base Price can not be Negative")
+            record.total_price = record.base_price + record.additional_fee
